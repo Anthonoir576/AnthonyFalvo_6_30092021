25587cfbd3d972129a44e3feabd4bccb4c42c680
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-30 14:44:48 +0200

    installation express/mangoose

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+/* ##########   MES DECLARATIONS   ################ */
+
+const express = require('express');
+
+
+const app = express();
+
+
+// exportation de la const app pour la recuperer sur nimporte quel fichier
+module.exports = app;
+/* ################################################ */
