bace5ab61518072a93f0c7235e7c332517ec78d4
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-09-30 14:54:36 +0200

    mise en place express

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,23 @@
 
 const express = require('express');
 
-
 const app = express();
 
+/* ################################################ */
+
+
+
+app.use((request, response) => {
+
+    response.json({message: 'ok'})
+
+});
+
+
+
+/* ##############    EXPORT     ################### */
 
 // exportation de la const app pour la recuperer sur nimporte quel fichier
 module.exports = app;
+
 /* ################################################ */
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,8 @@
 
 // const http qui permet l'accès a l'objet http et créer un serveur
 const http = require('http');
-
-// creatServer fait partie du package de http
-const server = http.createServer((request, response) => {
-
-    response.end('test')
-
-});
-
+// importation de app depuis app.js
+const app = require('./app');
 
 /* ################################################ */
 
@@ -19,6 +13,12 @@ const server = http.createServer((request, response) => {
 
 
 /* ################  SERVEUR  ##################### */
+
+// indication a express sur quel port tourner
+app.set('port', process.env.port || 3000);
+
+// creatServer fait partie du package de http
+const server = http.createServer(app);
 
 // Si l'environnement sur lequel tourne le serveur envoie un port a utilisé, ou sinon par defaut le port 3000
 server.listen(process.env.port || 3000);
