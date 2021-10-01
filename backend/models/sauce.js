3cab6eac9c56f23c9039188068ec180fcd84d843
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-10-01 09:28:19 +0200

    modele sauce a tester

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,5 +1,4 @@
 
-
 /* ##########   MES DECLARATIONS   ################ */
 
 // importation package => mongoose
@@ -9,15 +8,32 @@ const mongoose = require('mongoose');
 
 
 
-
 /* ################  SCHEMA  ###################### */
 
 // Fonction Schema fournis par le package mongoose permet de créer un schema type que l'on a besoin pour la création d'une nouvelle sauce
 const sauceSchema = mongoose.Schema({
 
-
+    userId: { type: String, required: true },
+    name: { type: String, required: true }, 
+    manufacturer: { type: String, required: true }, 
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true }, 
+    heat: { type: Number, required: true },
+    likes: { type: Number },
+    dislikes : { type: Number },
+    usersLiked : [ { type: String } ], 
+    usersDisliked : [ { type: String } ] 
 
 });
 
 /* ################################################ */
 
+
+
+/* ##############    EXPORT     ################### */
+
+// Export du shéma de sauce créer Sauce serra le nom de l'objet crée et saucheSchema le modèle a utilisé 
+module.exports = mongoose.model('Sauce', sauceSchema);
+
+/* ################################################ */
