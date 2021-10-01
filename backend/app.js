385bfde8279d6df4ea575f175d41dcf45a21067e
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-10-01 08:26:36 +0200

    utilisation de express.json() a la place de bodyparser qui est integrer mtn a express directement

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,50 @@
 /* ##########   MES DECLARATIONS   ################ */
 
+// import express
 const express = require('express');
 
+// application express
 const app = express();
 
 /* ################################################ */
 
+
+
+
+/* ##############    CORS   ####################### */
+// Permet notamment d'eviter les requêtes indésirables
+// Autorisation de l'utilisation de l'api grace au argument passé aux différents headers et evite la sécurité CORS 
 app.use((request, response, next) => {
 
-    console.log('requete recu');
+    // autorisation à '*' donc tout le monde :
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    // autorisation de certain en tête dans les requêtes :
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // autorisation de certaine methodes ou verbe attribué aux différentes requêtes :
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
     next();
 
 });
 
-app.use((request, response, next) => {
-
-    response.status(201);
-    next();
-
-});
-
-app.use((request, response, next) => {
-
-    response.json({message: 'ok'});
-    next();
-
-});
+/* ################################################ */
 
 
-app.use((request, response) => {
 
-    console.log('reponse envoyé succès');
-  
+/* #####  CONVERTION DU CORP DE LA REQUETE   ###### */
+
+// suite a la maj 4.16 bodyParser est intégrer a express
+// Permet de convertir le corp de la requête en objet json exploitable
+app.use(express.json());
+
+/* ################################################ */
+
+
+app.post('/api/stuff', (request, response, next) => {
+
+    console.log(request.body);
+    response.status(201).json({ message: 'salut'});
+
 });
 
 
