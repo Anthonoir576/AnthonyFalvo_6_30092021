5a3c5d1b97688d2048d9db4775070c4ff5f3dbca
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-10-28 11:20:39 +0200

    :docs: Ajoute .igniore .env

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const http = require('http');
 // importation de app depuis app.js
 const app = require('./app');
 
+// Importation de la dépendance dotenv
+const environnement = require('dotenv');
+environnement.config();
 /* ################################################ */
 
 
@@ -16,12 +19,12 @@ const app = require('./app');
 /* ################  SERVEUR  ##################### */
 
 // indication a express sur quel port tourner
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 
 // creatServer fait partie du package de http
 const server = http.createServer(app);
 
 // Si l'environnement sur lequel tourne le serveur envoie un port a utilisé, ou sinon par defaut le port 3000
-server.listen(process.env.port || 3000);
+server.listen(process.env.PORT || 3000);
 
 /* ################################################ */
